94bc274288d7ed0d84e5f86e14e5abc0ce607660
pankoivan <panivan98@yandex.ru>
2024-02-17 14:18:10 +0300

    Добавил контроллер и класс Info, содержащий вводимую в форму информацию. Исправил страницы ввода и вывода информации.

---
+++ src/main/java/com/distributed_systems_labs/Info.java
@@ -0,0 +1,14 @@
+package com.distributed_systems_labs;
+
+import lombok.Data;
+
+@Data
+public class Info {
+
+    private String userLastname;
+
+    private String userName;
+
+    private String userEmail;
+
+}
