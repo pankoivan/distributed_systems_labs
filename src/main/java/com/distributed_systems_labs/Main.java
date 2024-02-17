58cd914bb4663b8b327db7b14d765d9426fd3add
pankoivan <panivan98@yandex.ru>
2024-02-17 13:50:06 +0300

    Стартовый коммит

---
+++ src/main/java/com/distributed_systems_labs/Main.java
@@ -0,0 +1,13 @@
+package com.distributed_systems_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/test/java/com/distributed_systems_labs/MainTests.java
@@ -0,0 +1,13 @@
+package com.distributed_systems_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
