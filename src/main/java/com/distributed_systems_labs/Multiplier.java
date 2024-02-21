628f659d0e29a34a4d6a4172f41fd2aea48b28f8
pankoivan <panivan98@yandex.ru>
2024-02-21 14:54:34 +0300

    Добавил пропущенную аннотацию RequestBody в метод multiply().

--- src/main/java/com/distributed_systems_labs/Multiplier.java
+++ src/main/java/com/distributed_systems_labs/Multiplier.java
@@ -1,13 +1,14 @@
 package com.distributed_systems_labs;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Multiplier {
 
     @PostMapping("/multiply")
-    public Response multiply(Request request) {
+    public Response multiply(@RequestBody Request request) {
         return new Response(request.getAmount() * request.getRate());
     }
 
